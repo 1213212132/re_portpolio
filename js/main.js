5687d7c138b415a6914bb0c88e46494c4da15c5f
asdas <95152341+1213212132@users.noreply.github.com>
2023-08-25 10:43:18 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    $(".wrap").fullpage({
+        navigation: true,//네비게이션 유무
+        navigationPosition: "right",//네비게이션 위치
+        sectionsColor: [],//각 섹션 배경색, 배열의 형식
+        navigationTooltips: ["인트로", "NEWS", "3번", "4번"],//네비게이션 텍스트, 배열의 형식
+        showActiveTooltip: true,//네비게이션 선택 메뉴 활성화
+
+
+
+    });
+
+
+})
+
+
